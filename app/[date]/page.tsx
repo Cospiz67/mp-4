7a0d3c1d698878e0e58ed9798a9443afc4b571a6
Cospiz67 <jeanne.durand@epita.fr>
2026-03-22 00:43:10 -0400

    api call

--- app/[date]/page.tsx
+++ app/[date]/page.tsx
@@ -90,28 +90,41 @@ export default function Date(){
         }, [data]);
         console.log("helloooooooooo");
         console.log("data: "+ data?.title);*/
-    /*const {data, error} = useSWR(`./api/getNasaData?date=${params.date}`,
+    const {data, error} = useSWR(`./api/getNasaData?date=${params.date}`,
         (url)=> fetch(url).then((res)=>res.json())
-    );*/
-    const data= {
-    "copyright": "Panther Observatory",
-    "date": "2006-04-15",
-    "explanation": "In this stunning cosmic vista, galaxy M81 is on the left surrounded by blue spiral arms.  On the right marked by massive gas and dust clouds, is M82.  These two mammoth galaxies have been locked in gravitational combat for the past billion years.   The gravity from each galaxy dramatically affects the other during each hundred million-year pass.  Last go-round, M82's gravity likely raised density waves rippling around M81, resulting in the richness of M81's spiral arms.  But M81 left M82 with violent star forming regions and colliding gas clouds so energetic the galaxy glows in X-rays.  In a few billion years only one galaxy will remain.",
-    "hdurl": "https://apod.nasa.gov/apod/image/0604/M81_M82_schedler_c80.jpg",
-    "media_type": "image",
-    "service_version": "v1",
-    "title": "Galaxy Wars: M81 versus M82",
-    "url": "https://apod.nasa.gov/apod/image/0604/M81_M82_schedler_c25.jpg"
-  };
+    );
+//     const data= {
+//     "copyright": "Panther Observatory",
+//     "date": "2006-04-15",
+//     "explanation": "In this stunning cosmic vista, galaxy M81 is on the left surrounded by blue spiral arms.  On the right marked by massive gas and dust clouds, is M82.  These two mammoth galaxies have been locked in gravitational combat for the past billion years.   The gravity from each galaxy dramatically affects the other during each hundred million-year pass.  Last go-round, M82's gravity likely raised density waves rippling around M81, resulting in the richness of M81's spiral arms.  But M81 left M82 with violent star forming regions and colliding gas clouds so energetic the galaxy glows in X-rays.  In a few billion years only one galaxy will remain.",
+//     "hdurl": "https://apod.nasa.gov/apod/image/0604/M81_M82_schedler_c80.jpg",
+//     "media_type": "image",
+//     "service_version": "v1",
+//     "title": "Galaxy Wars: M81 versus M82",
+//     "url": "https://apod.nasa.gov/apod/image/0604/M81_M82_schedler_c25.jpg"
+//   };
+    const parts = (data?.url).split('.');
+    const extension = parts[parts.length-1];
+
     console.log(data);
     return(
         <StyledContainer>
             <StyledTexts>
                 <StyledH1>{data?.title} | {data?.date}</StyledH1>
                 <StyledP>{data?.explanation}</StyledP>
-                <StyledCredits>Copyright: {data?.copyright}</StyledCredits>
+                <StyledCredits>Copyright: {data?.copyright ===undefined? "Not known":data?.copyright}</StyledCredits>
                 <StyledButton onClick={()=>window.location.href = "/"}>Return to Date Selector</StyledButton>
             </StyledTexts>
+            {/* {
+                if(extension ==="mp4")
+                {<>
+                    return(
+                        <video src= {data?.url}></video>
+                    );
+                    </>
+                }
+
+            } */}
             <StyledImg src= {data?.url} alt={data?.title}></StyledImg>
         </StyledContainer>
     )
